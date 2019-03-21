c4fa0a9b2daa7ab12a344bc59ae30ca387c1c462
Oscar Shoudt <shoudto@gmail.com>
2019-03-21 15:24:59 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe "#introduction" do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     def introduction(name)
+      puts "Hi, my name is #{name}. "
+    end
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
 end
